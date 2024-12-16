f6b610b7f503f4b5c1acea90934744b6f610efbe
barmud3 <barmud9798@gmail.com>
2024-12-16 23:55:39 +0200

    fully working minigame

---
+++ Assets/Scripts/ballonCounter.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CandyCounter : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI ballons;
+    [SerializeField] private int score = 0;
+
+    // private void Start() {
+    //     candies = GameObject.Find("candyCounter").GetComponent<Text>();
+    // }
+
+    public void AddBallon(int amount){
+        score += amount;
+        UpdateScoreUI();
+    }
+    private void UpdateScoreUI(){
+        ballons.text = "      X " + score;
+
+    }
+}
--- Assets/Scripts/ballonHit.cs
+++ Assets/Scripts/ballonHit.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 public class ballonHit : LaserInteractable
 {
    [SerializeField] private string uniqueBehaviorMessage = "Specific behavior triggered!";
+    private CandyCounter candyCounter;
 
+    private void Start() {
+		candyCounter = Object.FindFirstObjectByType<CandyCounter>();
+	}
 
    // Override the OnLaserHit to add specific behavior and stop laser reflection
    public override void OnLaserHit(ref bool stopRay) {
       base.OnLaserHit(ref stopRay); // Perform base behavior (sprite change, etc.)
-
+      candyCounter.AddBallon(1);
       // Log and perform specific behavior
       Debug.Log(uniqueBehaviorMessage);
           // If a new sprite is assigned, change the object's sprite
