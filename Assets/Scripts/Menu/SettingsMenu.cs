c670773e0c61b5fdb9bf2090a13a3c59b2feb041
ron_shuster <ronshuster99@gmail.com>
2025-01-28 23:43:16 +0200

    Replacing Mirrors with Crystals

--- Assets/Scripts/Menu/SettingsMenu.cs
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -4,19 +4,24 @@ public class SettingsMenu : Panel
 {
     public override void Open()
     {
-        if (PanelManager.IsOpen("loading"))
-        {
-            PanelManager.Close("loading");
+        // if (PanelManager.IsOpen("loading"))
+        // {
+        //     PanelManager.Close("loading");
 
-            base.Open();
-            PanelManager.Open("loading");
-        }
-        PanelManager.Close("loading");
-        PanelManager.Close("auth");
-        PanelManager.Close("error");
-        PanelManager.Close("main");
+        //     base.Open();
+        //     PanelManager.Open("loading");
+        // }
+        // PanelManager.Close("loading");
+        // PanelManager.Close("auth");
+        // PanelManager.Close("error");
+        // PanelManager.Close("main");
 
         base.Open();
     }
-    
+
+    public override void Close()
+    {
+        base.Close();
+    }
+
 }
